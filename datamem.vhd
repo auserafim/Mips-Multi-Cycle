7c2c9e1bb5c3844291e9868ae1002fa0efe5202b
Aurélio Serafim <128054245+auserafim@users.noreply.github.com>
2024-10-16 12:44:54 -0300

    Add files via upload

--- datamem.vhd
+++ datamem.vhd
@@ -1,32 +1,55 @@
 library IEEE; 
 use IEEE.STD_LOGIC_1164.all; 
-use STD.TEXTIO.all;
 use IEEE.NUMERIC_STD.all; 
 
 entity datamem is 
-  port(clk, we:  in STD_LOGIC;
-       a, wd:    in STD_LOGIC_VECTOR(31 downto 0);
-       rd:       out STD_LOGIC_VECTOR(31 downto 0));
-end;
+  port(
+    clk    : in  STD_LOGIC;
+    we     : in  STD_LOGIC;              -- Write enable for data
+    a      : in  STD_LOGIC_VECTOR(31 downto 0); -- Address
+    wd     : in  STD_LOGIC_VECTOR(31 downto 0); -- Write data
+    rd     : out STD_LOGIC_VECTOR(31 downto 0) -- Read data
+  );
+end datamem;
 
 architecture synth of datamem is
-   -- Array de memória
-   type ram_type is array (63 downto 0) of STD_LOGIC_VECTOR(31 downto 0);
-	signal mem: ram_type := (others => (others => '0')); -- inicializada com zeros
+   type ram_type is array (127 downto 0) of STD_LOGIC_VECTOR(31 downto 0); -- max adress would be 0x200 or 512 in binary
+
+    -- instructions
+    signal mem: ram_type := (
+           0  => x"20020005", -- first word receives this instruction
+           1  => x"2003000c",
+           2  => x"2067fff7",
+           3  => x"00e22025",
+           4  => x"00642824",
+           5  => x"00a42820",
+           6  => x"10a7000a",
+           7  => x"0064202a",
+           8  => x"10800001",
+           9  => x"20050000",
+           10 => x"00e2202a",
+           11 => x"00853820",
+           12 => x"00e23822",
+           13 => x"ac670044",
+           14 => x"8c020050",
+           15 => x"08000011",
+           16 => x"20020001",
+           17 => x"ac020054", -- 0x44 adress  
+           others => (others => '0') -- start all memory positions with zeros 
+    );
+
+
 begin
-  -- na borda de subida, se we = '1', escreve na palavra de memória endereçada
-  -- pelo sinal a
   process(clk)
   begin
-     if rising_edge(clk) then
-        if (we = '1') then
-           -- endereços de palavras avançam de 4 em 4. Por isso, os
-	   -- 2 bits menos significativos de 'a' sao desconsiderados.  
-           mem(to_integer(unsigned(a(7 downto 2)))) <= wd;
-        end if;
-     end if;
+    if rising_edge(clk) then
+      if (we = '1') then
+        mem(to_integer(unsigned(a(8 downto 2)))) <= wd; -- 7 bits are enough to adress 128 words 
+      end if;
+    end if;
   end process;
-  
-  -- Concorrentemente lê a palavra endereçada por a
-  rd <= mem(to_integer(unsigned(a(7 downto 2)))); 
-end synth;
+
+  -- Read operation, asynchronous
+  rd <= mem(to_integer(unsigned(a(8 downto 2)))); -- 7 bits are enough to adress 128 words 
+
+end architecture synth;
